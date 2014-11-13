164b990f5ddc3fc270be65dbb956e16b5b59c84c
Vishal Kumar <vishal.rgiit@gmail.com>
2014-11-13 11:27:01 +0530

    send dynamic html

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+
+app.set('views', __dirname + '/templates');
+
+// app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
+
+app.get('/greet', function(request, response) {
+  var name = request.query.name;
+
+  response.render('greet-dynamic', {
+    name: name
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
